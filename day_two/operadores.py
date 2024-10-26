619eda527a27fac3f750d8630b8e452a58ef7f48
Dorian Gonzalez <doriandarren1@gmail.com>
2024-10-26 21:43:25 +0200

    funciones aritmetica

---
+++ day_two/operadores.py
@@ -0,0 +1,15 @@
+x = 6
+y = 2
+z = 7
+
+
+print(f"{x} + {y} = {x+y}")
+print(f"{x} - {y} = {x-y}")
+print(f"{x} * {y} = {x*y}")
+print(f"{x} / {y} = {x/y}")
+
+print(f"{z} redondeo hacia abajo {y} = {z//y}") ## redondeo hacia abajo
+print(f"{z} resto {y} = {z%y}")  ## resto
+print(f"{x} elevado {y} = {x**y}")  ## potencia 2
+print(f"{x} elevado {3} = {x**3}")  ## potencia 3
+print(f"{x} raiz {x} = {x**0.5}")  ## raiz cuadrada
