de063ca48c2b9ce604fa7d853d5a0996311451de
Dorian Gonzalez <doriandarren1@gmail.com>
2024-10-26 21:35:34 +0200

    print

--- day_two/formatear_cadenas.py
+++ day_two/formatear_cadenas.py
@@ -1,11 +1,14 @@
-
 x = 10
 y = 5
 
-
 print("Mis numeros son " + str(x) + " " + str(y))
-
 print("Mis numeros son {} y {} es igual a: {}".format(x,y,x+y))
-
-
 print(f"Mis numeros son {x} y {y}")
+
+
+
+
+color = "rojo"
+matricula = 546543
+
+print(f"el auto es: {color}, matrícula: {matricula}")
