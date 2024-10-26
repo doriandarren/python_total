e17c927302b9072046a94a7becea2cb35ac35e98
Dorian Gonzalez <doriandarren1@gmail.com>
2024-10-26 20:21:16 +0200

    first commit

---
+++ day_one/Combine.py
@@ -0,0 +1,6 @@
+
+
+palabra1 = input("Que ciudad te gustaria visitar?: ")
+palabra2 = input("Cual es tu color favorito?: ")
+
+print("Marca de la cerveza:'" + palabra1 + " " + palabra2 + "'")
---
+++ day_one/Primer_Proyecto.py
@@ -0,0 +1,10 @@
+# print("Hola" + " " + 'Mundo')
+# print("Esta es una linea\nY esta es otra linea")
+# print("\tEsta es la tercera linea")
+# print('This isn\'t a number')
+
+
+print("tu nombre es: " + input("Nombre: ") + " " + input("Apellido: "))
+
+
+
