d094fda0a3ac2d6b14b9825e71d7e1e72d7dcc6b
Dorian Gonzalez <doriandarren1@gmail.com>
2024-11-15 23:01:19 +0100

    add rocket

--- day_10/main.py
+++ day_10/main.py
@@ -14,12 +14,30 @@ icono = pygame.image.load("ovni.png")
 pygame.display.set_icon(icono)
 
 
+
+# Jugador
+img_jugador = pygame.image.load("cohete.png")
+jugador_x = 368 # Se calcula del tamaño 800 / 2 = 400 y hay que restar el tamaño en pixel de la imagen que se descargo
+jugador_y = 536
+
+def jugador(x,y):
+    pantalla.blit(img_jugador, (x, y))
+
+
+
 # Loop del juego
 se_ejecuta = True
 while se_ejecuta:
+
+    # RGB
+    pantalla.fill((205, 144, 228))
+    jugador_x += 0.1
+
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             se_ejecuta = False
 
-    pantalla.fill((205, 144, 228))
+
+    jugador(jugador_x, jugador_y)
+
     pygame.display.update()
